72c29c6165bf91b5059c386e3442ffe9f3696689
Praveenkumar V <pkvpraveen@gmail.com>
2015-09-19 20:42:39 +0530

    Gallary

--- js/freelancer.js
+++ js/freelancer.js
@@ -6,6 +6,9 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 (function () {
+    var filenames =[];
+    var imageMin = '';
+    var index=0;
     $(function () {
         $('body').on('click', '.page-scroll a', function (event) {
             var $anchor = $(this);
@@ -14,29 +17,53 @@
             }, 1500, 'easeInOutExpo');
             event.preventDefault();
         });
-        // Initialize WOW.js Scrolling Animations
-    });
 
-// Floating label headings for the contact form
-    $(function () {
-        $("body").on("input propertychange", ".floating-label-form-group", function (e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function () {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function () {
-            $(this).removeClass("floating-label-form-group-with-focus");
+//for galary
+        var fileExt = {};
+        $.ajax({
+            url: '../img/portfolio/',
+            success: function (data) {
+                //List all png or jpg or gif file names in the page
+                $(data).find('a:contains(".png"),a:contains(".jpg"),a:contains(".gif")').each(function () {
+                    filenames.push(this.href.replace(window.location.host, "").replace("http:///", ""));
+                });
+                showFourImages();
+            }
         });
     });
+
+    $('#portfolio-more').click(function () {
+        showFourImages();
+    });
+
+    function showFourImages() {
+        var currentIndex = index;
+        for (var i = currentIndex; i < currentIndex + 4; i++) {
+            index += 1;
+            imageMin += '<div class="col-sm-3 col-xs-12">' +
+                '<a href="img/portfolio/' + filenames[i] + '" data-title="Installations" data-lightbox="installations">' +
+                '<img src="img/portfolio/' + filenames[i] + '" class="img-thumbnail lazyload" alt=""/>' +
+                '</a>' +
+                '<div class="caption"> </div>' +
+                '</div>';
+        }
+        $("#portfolio-container").html(imageMin);
+        if (index >= filenames.length) {
+            $('#portfolio-more').hide();
+        }
+    }
 
 // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top'
     });
 
-
 // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
+
+    // Initialize WOW.js Scrolling Animations
     new WOW().init();
+
 }());
